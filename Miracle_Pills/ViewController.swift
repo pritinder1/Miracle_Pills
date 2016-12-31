e0dc46ab232ccba41bd580df4fa16e8163819360
pritinder1 <Pritinder@pritinders-mbp.home>
2016-12-31 17:54:25 -0500

    Initial Commit

---
+++ Miracle_Pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Miracle_Pills
+//
+//  Created by Pritinder Singh  on 12/31/16.
+//  Copyright © 2016 Pritinder Singh . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
